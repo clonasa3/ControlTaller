f33517d5712e0a87b84f372e3c981882e1dd3b7a
Arfeon <pclota27@gmail.com>
2019-03-22 19:10:31 +0100

    Nous Comentaris Al Main per fer test

--- ControlTaller/src/application/Main.java
+++ ControlTaller/src/application/Main.java
@@ -7,13 +7,16 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.fxml.FXMLLoader;
 
-
+/**
+ * Inici del programa amb JavaFx carrega la vista de IniciTaller.fxml
+ * @author pclot
+ *
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/vistes/IniciTaller.fxml"));
-			//root.getChildren().add(FXMLLoader.load(getClass().getResource("/vistes/IniciCercaClients.fxml")));
 			Scene scene = new Scene(root,600,250);			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
